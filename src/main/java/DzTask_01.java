9a17d5ec9e387d4e6d0428a7f1b56f6e6adf3243
Daria <shkatov.pad@gmail.com>
2023-06-15 17:11:42 +0300

    klkllkk

--- src/main/java/DzTask_01.java
+++ src/main/java/DzTask_01.java
@@ -3,9 +3,13 @@ public class DzTask_01 {
     static int num() {
          return new Random().nextInt(2000);
     }
-
+    static int[] lol(int length) {
+        int[] arr = new int[length];
+        return arr;
+    }
     public static void main(String[] args) {
         int i = num();
         System.out.println(i);
+
     }
 }
