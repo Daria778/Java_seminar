36cc4b7ef97295f852a7252c6e78bf8323089e28
Daria <shkatov.pad@gmail.com>
2023-06-20 19:45:33 +0300

    эх

--- src/main/java/DzTask_02.java
+++ src/main/java/DzTask_02.java
@@ -1,17 +1,62 @@
 import java.util.Scanner;
 public class DzTask_02 {
+    static void lol() {
+        StringBuilder lol = new StringBuilder(77);
+        String filter = "name : Ivanov, country : Russia, city : Moscow, age : null";
+        filter = filter.replace(":", "=");
+        lol.append("filter : ").append(filter);
+        System.out.println(lol);
+    }
 
-
-    public static void main(String[] args) {
+    static StringBuilder joke() {
         Scanner scanner = new Scanner(System.in);     //Вводятся значения из задачи
         System.out.print("Enter the surname: ");
         String surname = scanner.nextLine();
+
         System.out.print("Enter the mark: ");
         String mark = scanner.nextLine();
+
         System.out.print("Enter the subject: ");
         String subject =scanner.nextLine();
-        String str = "Студент" + " " + surname + " " + "получил" + " " + mark + " " + "по предемету" + " " + subject;
-        System.out.println(str);
+
+        StringBuilder str = new StringBuilder(90);
+        str.append("Студент ").append(surname).append(" получил ").append(mark).append(" по предмету ").append(subject).append(".");
+        return str;
+    }
+    static void method() {
+        String s = "";
+        StringBuilder builder1 = new StringBuilder("");
+
+        for (int i = 0; i < 1011; i++) {
+            s += "a";
+        }
+        for (int i = 0; i < 1011; i++) {
+            builder1.append("a");
+        }
+        long start = System.currentTimeMillis();
+        s = s.replace("a", "A");
+        System.out.println(s);
+        System.out.println("String time = " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        StringBuilder lol = new StringBuilder("");
+        for (int i = 0; i < 1011; i++) {
+            lol.append("A");
+        }
+        builder1 = lol;
+        System.out.println("StringBuilder time = " + (System.currentTimeMillis() - start));
+        System.out.println(builder1);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(joke());
+        lol();
+        method();
+
+        String str = ".3 + 1.85";
 
     }
-}
+
+    }
