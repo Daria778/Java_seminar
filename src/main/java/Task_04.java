9c6bd17fadb823dca1dac9d3748a4d47b8797e4e
Daria <shkatov.pad@gmail.com>
2023-06-25 20:42:09 +0300

    data

--- src/main/java/Task_04.java
+++ src/main/java/Task_04.java
@@ -1,27 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Task_04 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the surname: ");
-        String surname = scanner.nextLine();
-        System.out.print("Enter the name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter the patronymic: ");
-        String patronymic = scanner.nextLine();
-        System.out.print("Enter the sex: ");
-        String sex = scanner.nextLine();
-        System.out.print("Enter the age: ");
-        String age = scanner.nextLine();
+        System.out.print("The amount of people is: ");
+        int num = Integer.parseInt(scanner.nextLine());
+        ArrayList<String> people = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            System.out.print("Enter the surname: ");
+            String surname = scanner.nextLine();
+            System.out.print("Enter the name: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter the patronymic: ");
+            String patronymic = scanner.nextLine();
+            System.out.print("Enter the sex: ");
+            String sex = scanner.nextLine();
+            System.out.print("Enter the age: ");
+            String age = scanner.nextLine();
+            String human = surname + " " + name + " " + patronymic + " " + age + " " + sex;
+            people.add(human);
+        }
+        for (int i = 0; i < people.size(); i++) {
+            String[] n = people.get(i).split(" ");
+        }
 
+        people.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o1.split(" ")[3]) - Integer.parseInt(o2.split(" ")[3]);
+            }
+        });
 
-        ArrayList <String> people= new ArrayList<>();
-        people.add(surname);
-        people.add(name);
-        people.add(patronymic);
-        people.add(sex);
-        people.add(age);
-        System.out.printf("%s %s. %s. %s, %s", surname, name.charAt(0), patronymic.charAt(0), age, sex);
+        for (int i = 0; i < people.size(); i++) {
+            String[] n = people.get(i).split(" ");
+            System.out.println(n[0] + " " + n[1].charAt(0) + "." + n[2].charAt(0)+ "." +n[3] + " " + n[4]);
+        }
     }
 }
