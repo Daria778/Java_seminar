547b0e7ced65a72b5fb2a1e4f03d915d7543af9e
Daria <shkatov.pad@gmail.com>
2023-06-28 16:53:25 +0300

    oipi

--- src/main/java/DzTask_05.java
+++ src/main/java/DzTask_05.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class DzTask_05 {
@@ -5,9 +7,48 @@ public class DzTask_05 {
         HashMap<String, String> tel = new HashMap<>();
         tel.merge("lol", "12345", (old, n) -> old + ";" + n);
         tel.merge("lol", "9875645", (old, n) -> old + ";" + n);
+        tel.merge("pop", "9875645", (old, n) -> old + ";" + n);
+        tel.merge("ted", "9875645", (old, n) -> old + ";" + n);
+        tel.merge("pop", "9875645", (old, n) -> old + ";" + n);
         System.out.println(tel);
-            }
+        ArrayList<String> l = new ArrayList<>();
+        for (String i: tel.keySet()) {
+            l.add(i);
         }
+        l.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return tel.get(o1).split(";").length - tel.get(o2).split(";").length;
+            }
+        });
+        System.out.println(l);
+
+
+    }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
